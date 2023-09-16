e8b1709836f95f6aa8db94643ef1807e42c6f420
A Gyuri <86420585+AzAGyuri@users.noreply.github.com>
2023-09-16 21:50:11 +0200

    Add files via upload

--- fooldal.js
+++ fooldal.js
@@ -1,4 +1,4 @@
-alert("Ezen az oldal a 2 bekezdés szövegére kattintva a szöveg színe randomizáltan változik!\nPróbálja ki!");
+alert("Ezen az oldal a 2 bekezdés szövegére kattintva a szöveg színe randomizáltan változik!\nPróbálja ki!\n+Várjon 30 mp-t a szem animációjával!");
 function szoveg(){
     document.getElementById("fooldal").style.color="#"+Math.floor(Math.random()*16777215).toString(16);
 }
