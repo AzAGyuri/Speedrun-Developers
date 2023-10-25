423380bdd1ed3e20ec97ae51dbda37561787e9e4
zagyi.krisztian0925@gmail.com <zagyi.krisztian0925@gmail.com>
2023-10-25 17:39:57 +0200

    Vétel oldal megcsinálva asszem

--- aukcios-haz/scripts/vetel.js
+++ aukcios-haz/scripts/vetel.js
@@ -1,2 +1,24 @@
+var osszes = 0;
 
+function addToOsszes(value) {
+    osszes += value;
+}
+
+function showTotal() {
+    if (osszes === 0) {
+        alert('Jelenleg még nincs termék a kosárban, kérem válassza ki a vásárolni kívánt terméket!');
+    } else {
+        var confirmation = confirm('Az ár: $' + osszes + '\nKívánja megvásárolni a termékeket?');
+
+        if (confirmation) {
+            var purchaseConfirmation = confirm('Sikeres vásárlás');
+            if (purchaseConfirmation) {
+                osszes = 0;
+            }
+        } else {
+            alert('Törölt minden terméket');
+            osszes = 0;
+        }
+    }
+}
 
