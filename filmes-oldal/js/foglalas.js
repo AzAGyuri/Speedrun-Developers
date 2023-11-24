e50dd34807b186252bafb59d4f13d45eb4886878
SpeedRunHunter <marcell2003.sm@gmail.com>
2023-11-24 09:44:21 +0100

    Yep fájl kezelés

---
+++ filmes-oldal/js/foglalas.js
@@ -0,0 +1,4 @@
+const uploadButton = document.getElementById('uploadButton');
+        uploadButton.addEventListener('click', () => {
+            alert(`Sikeresen megvásárolta a jegyeit a filmre!!`);
+        });
