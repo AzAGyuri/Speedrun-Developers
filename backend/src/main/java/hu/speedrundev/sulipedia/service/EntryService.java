62c5c42ab99fec13b8990f5054a7f35e4f6b1a12
SpeedRunHunter <marcell2003.sm@gmail.com>
2024-04-12 19:48:56 +0200

    most valamiért kellenek ezek az transactional annotációk mindegy, ha neki így kell, megkapja

--- backend/src/main/java/hu/speedrundev/sulipedia/service/EntryService.java
+++ backend/src/main/java/hu/speedrundev/sulipedia/service/EntryService.java
@@ -33,9 +33,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@Transactional
 public class EntryService {
 
   @Autowired
--- backend/src/main/java/hu/speedrundev/sulipedia/service/UserService.java
+++ backend/src/main/java/hu/speedrundev/sulipedia/service/UserService.java
@@ -21,8 +21,10 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class UserService {
 
   @Autowired
