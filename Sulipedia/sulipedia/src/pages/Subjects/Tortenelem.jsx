230a0728d69025bb53465fd478ee970e64dc8c13
A Gyuri <polyak.gyorgy21@protonmail.com>
2024-04-13 14:26:16 +0200

    Történelem Tananyag oldal in progress

--- Sulipedia/sulipedia/src/pages/Subjects/Tortenelem.jsx
+++ Sulipedia/sulipedia/src/pages/Subjects/Tortenelem.jsx
@@ -60,14 +60,6 @@ const StyledList = styled(List)({
   width: "100%",
 });
 
-const StyledListItem = styled(ListItem)({
-  borderBottom: "1px solid #7ffc03",
-});
-
-const StyledListItemText = styled(ListItemText)({
-  color: "white",
-});
-
 const CommentSection = styled("div")({
   margin: "0 auto",
   marginTop: (theme) => theme.spacing(3),
@@ -140,25 +132,61 @@ const CommentDate = styled("span")({
   marginLeft: "6px",
 });
 
+function Entry({ title, content, date, author }) {
+  return (
+    <StyledContainer style={{ backgroundColor: "#4caf50" }}>
+      <Title variant="h4">{title}</Title>
+      <LargeText>{content}</LargeText>
+      <div style={{ display: "flex", justifyContent: "space-between", width: "100%", borderTop: "2px solid #2f3826", marginTop: "auto", paddingRight: "16px", paddingLeft: "16px" }}>
+        <Typography variant="body2" style={{ padding: "8px 0", backgroundColor: "#ba8d63", borderRadius: "5px", color: "#fff", fontWeight: "bold" }}>{new Date(date).toLocaleDateString()}</Typography>
+        <Typography variant="body2" style={{ padding: "8px 0", backgroundColor: "#6384ba", borderRadius: "5px", color: "#fff", fontWeight: "bold" }}>{author}</Typography>
+      </div>
+    </StyledContainer>
+  );
+}
 export function Tortenelem({ children }) {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
-  const [showSubMenu1, setShowSubMenu1] = React.useState(false);
-  const [showSubMenu2, setShowSubMenu2] = React.useState(false);
-  const [showSubMenu3, setShowSubMenu3] = React.useState(false);
-  const [showSubMenu4, setShowSubMenu4] = React.useState(false);
-
-  const [showSubSubMenu1, setShowSubSubMenu1] = React.useState(false);
-  const [showSubSubMenu2, setShowSubSubMenu2] = React.useState(false);
-  const [showSubSubMenu3, setShowSubSubMenu3] = React.useState(false);
-  const [showSubSubMenu4, setShowSubSubMenu4] = React.useState(false);
+   const entries = [
+    {
+      title: "Az Árpád-ház kora",
+      content: "A magyar történelem kezdete",
+      date: "2023.01.15",
+      author: "Történész1",
+      category: "HISTORY",
+    },
+    {
+      title: "A Mohácsi csata",
+      content: "Magyarország elvesztette függetlenségét",
+      date: "2023.02.03",
+      author: "Történész2",
+      category: "HISTORY",
+    },
+    {
+      title: "A Rákóczi-szabadságharc",
+      content: "Az összmagyar felkelés a Habsburgok ellen",
+      date: "2023.03.15",
+      author: "Történész3",
+      category: "HISTORY",
+    },
+    {
+      title: "Az 1848-49-es forradalom és szabadságharc",
+      content: "Az osztrák uralom elleni küzdelem",
+      date: "2023.04.04",
+      author: "Történész4",
+      category: "HISTORY",
+    },
+    {
+      title: "Az első világháború utáni Magyarország",
+      content: "A trianoni békeszerződés következményei",
+      date: "2023.05.20",
+      author: "Történész5",
+      category: "HISTORY",
+    },
+  ];
 
   const [mainMenuText, setMainMenuText] = React.useState(
     "Az egyes történelmi események és korszakok felfedezése létfontosságú a szilárd történelmi megértéshez. Merüljünk el néhány alapvető magyar történelmi témában."
   );
-
-  const [subMenuText, setSubMenuText] = React.useState("");
-
-  const [subSubMenuItemText, setSubSubMenuItemText] = React.useState("");
   const [comments, setComments] = React.useState([]);
   const [newComment, setNewComment] = React.useState("");
 
@@ -224,105 +252,13 @@ export function Tortenelem({ children }) {
     setDrawerOpen(!drawerOpen);
   };
 
-  const handleMenuItemClick = (menuItem) => {
-    setShowSubMenu1(false);
-    setShowSubMenu2(false);
-    setShowSubMenu3(false);
-    setShowSubMenu4(false);
-    setShowSubSubMenu1(false);
-    setShowSubSubMenu2(false);
-    setShowSubSubMenu3(false);
-    setShowSubSubMenu4(false);
-
-    switch (menuItem) {
-      case 1:
-        setShowSubMenu1(!showSubMenu1);
-        setSubMenuText(
-          "Az ókori magyar törzsrendszerek és a honfoglalás ideje. Ismerje meg az Árpád-ház kialakulásának és az első magyar állam megalapításának történetét."
-        );
-        setMainMenuText(
-          "Az ókori magyar történelem tanulmányozása feltárja az Árpád-ház és a magyar állam kialakulásának kritikus eseményeit."
-        );
-        break;
-      case 2:
-        setShowSubMenu2(!showSubMenu2);
-        setSubMenuText(
-          "A középkori Magyar Királyság és az Anjou-kor. Ismerje meg a középkori Magyarország társadalmi, politikai és kulturális fejleményeit."
-        );
-        setMainMenuText(
-          "A középkori Magyar Királyság történetének megértése betekintést nyújt a kor társadalmi szerkezetébe."
-        );
-        break;
-      case 3:
-        setShowSubMenu3(!showSubMenu3);
-        setSubMenuText(
-          "A török uralom és a Habsburg Monarchia időszaka Magyarországon. Ismerje meg a török háborúk, a Mohácsi csata és a Habsburg uralom hatásait."
-        );
-        setMainMenuText(
-          "A török uralom és a Habsburg Monarchia időszaka fontos eseményeket hozott Magyarország történetében."
-        );
-        break;
-      case 4:
-        setShowSubMenu4(!showSubMenu4);
-        setSubMenuText(
-          "Az 1956-os forradalom és az ezt követő időszak. Ismerje meg a magyar forradalom eseményeit és az ország történelmi fejlődését a 20. század második felében."
-        );
-        setMainMenuText(
-          "Az 1956-os forradalom és az azt követő időszak kulcsfontosságú volt Magyarország modern történelmében."
-        );
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleSubMenuItemClick = (subMenuItem) => {
-    switch (subMenuItem) {
-      case 1:
-        setShowSubSubMenu1(!showSubSubMenu1);
-        setSubSubMenuItemText(
-          "Az Árpád-ház és az első magyar állam kialakulásának folyamata. Ismerje meg a honfoglalás korszakát és az államalapító Szent István uralkodását."
-        );
-        setSubMenuText(
-          "Az ókori magyar történelem mélyebb megértése segít felfedezni az Árpád-ház és az első magyar állam létrejöttének kulcsfontosságú tényezőit."
-        );
-        break;
-      case 2:
-        setShowSubSubMenu2(!showSubSubMenu2);
-        setSubSubMenuItemText(
-          "A középkori Magyar Királyság társadalmi és gazdasági jellemzői. Az Anjou-kor kulturális és politikai hatásai a magyar történelem alakulására."
-        );
-        setSubMenuText(
-          "A középkori Magyar Királyság történelmi részleteinek megértése lehetővé teszi a kor társadalmi és gazdasági jellemzőinek jobb feltárását."
-        );
-        break;
-      case 3:
-        setShowSubSubMenu3(!showSubSubMenu3);
-        setSubSubMenuItemText(
-          "A török uralom kihívásai és a Habsburg Monarchia befolyása. A Mohácsi csata és a török háborúk hatásai Magyarországon."
-        );
-        setSubMenuText(
-          "A török uralom és a Habsburg Monarchia időszakának tanulmányozása segít megérteni a török háborúk és a Habsburg befolyás hatásait Magyarországon."
-        );
-        break;
-      case 4:
-        setShowSubSubMenu4(!showSubSubMenu4);
-        setSubSubMenuItemText(
-          "Az 1956-os forradalom eseményei és az azt követő időszak változásai. Az 56-os események hatása a hidegháborús időszakban."
-        );
-        setSubMenuText(
-          "Az 1956-os forradalom és az azt követő időszak részletes vizsgálata árnyaltabb képet ad a modern Magyarország kialakulásáról."
-        );
-        break;
-      default:
-        break;
-    }
-  };
+ 
+  
 
   return (
     <>
       {children}
-      <Tooltip title="Több tananyag ebben a témában">
+      <Tooltip title="Szűrés készítő szerint">
         <StyledDrawerButton onClick={toggleDrawer}>
           <MenuIcon />
         </StyledDrawerButton>
@@ -333,136 +269,28 @@ export function Tortenelem({ children }) {
         onClose={() => setDrawerOpen(false)}
       >
         <StyledList>
-          <StyledListItem button onClick={() => handleMenuItemClick(1)}>
-            <StyledListItemText primary="Ókori Magyar Történelem" />
-          </StyledListItem>
-          <Collapse in={showSubMenu1} timeout="auto" unmountOnExit>
-            <StyledList
-              component="div"
-              disablePadding
-              style={{ paddingLeft: "20px" }}
-            >
-              <StyledListItem button onClick={() => handleSubMenuItemClick(1)}>
-                <StyledListItemText primary="Árpád-ház és Honfoglalás" />
-              </StyledListItem>
-              <Collapse in={showSubSubMenu1} timeout="auto" unmountOnExit>
-                <StyledList
-                  component="div"
-                  disablePadding
-                  style={{ paddingLeft: "20px" }}
-                >
-                  <StyledListItem button>
-                    <StyledListItemText primary="Szent István Uralkodása" />
-                  </StyledListItem>
-                  <StyledListItem button>
-                    <StyledListItemText primary="Államalapítás Folyamata" />
-                  </StyledListItem>
-                </StyledList>
-              </Collapse>
-            </StyledList>
-          </Collapse>
-          <StyledListItem button onClick={() => handleMenuItemClick(2)}>
-            <StyledListItemText primary="Középkori Magyarország" />
-          </StyledListItem>
-          <Collapse in={showSubMenu2} timeout="auto" unmountOnExit>
-            <StyledList
-              component="div"
-              disablePadding
-              style={{ paddingLeft: "20px" }}
-            >
-              <StyledListItem button onClick={() => handleSubMenuItemClick(2)}>
-                <StyledListItemText primary="Magyar Királyság és Anjou-kor" />
-              </StyledListItem>
-              <Collapse in={showSubSubMenu2} timeout="auto" unmountOnExit>
-                <StyledList
-                  component="div"
-                  disablePadding
-                  style={{ paddingLeft: "20px" }}
-                >
-                  <StyledListItem button>
-                    <StyledListItemText primary="Társadalmi és Gazdasági Jellemzők" />
-                  </StyledListItem>
-                  <StyledListItem button>
-                    <StyledListItemText primary="Kulturális Hatások" />
-                  </StyledListItem>
-                </StyledList>
-              </Collapse>
-            </StyledList>
-          </Collapse>
-          <StyledListItem button onClick={() => handleMenuItemClick(3)}>
-            <StyledListItemText primary="Török Uralom és Habsburg Monarchia" />
-          </StyledListItem>
-          <Collapse in={showSubMenu3} timeout="auto" unmountOnExit>
-            <StyledList
-              component="div"
-              disablePadding
-              style={{ paddingLeft: "20px" }}
-            >
-              <StyledListItem button onClick={() => handleSubMenuItemClick(3)}>
-                <StyledListItemText primary="Török Háborúk és Mohácsi Csata" />
-              </StyledListItem>
-              <Collapse in={showSubSubMenu3} timeout="auto" unmountOnExit>
-                <StyledList
-                  component="div"
-                  disablePadding
-                  style={{ paddingLeft: "20px" }}
-                >
-                  <StyledListItem button>
-                    <StyledListItemText primary="Habsburg Befolyás" />
-                  </StyledListItem>
-                </StyledList>
-              </Collapse>
-            </StyledList>
-          </Collapse>
-          <StyledListItem button onClick={() => handleMenuItemClick(4)}>
-            <StyledListItemText primary="1956-os Forradalom és Az Utána Következő Időszak" />
-          </StyledListItem>
-          <Collapse in={showSubMenu4} timeout="auto" unmountOnExit>
-            <StyledList
-              component="div"
-              disablePadding
-              style={{ paddingLeft: "20px" }}
-            >
-              <StyledListItem button onClick={() => handleSubMenuItemClick(4)}>
-                <StyledListItemText primary="Forradalom Eseményei" />
-              </StyledListItem>
-              <Collapse in={showSubSubMenu4} timeout="auto" unmountOnExit>
-                <StyledList
-                  component="div"
-                  disablePadding
-                  style={{ paddingLeft: "20px" }}
-                >
-                  <StyledListItem button>
-                    <StyledListItemText primary="Hidegháborús Hatások" />
-                  </StyledListItem>
-                </StyledList>
-              </Collapse>
-            </StyledList>
-          </Collapse>
+         
+         
         </StyledList>
       </StyledDrawer>
 
-      <StyledContainer>
-        <Title variant="h3">A Magyar Történelemről</Title>
+      <StyledContainer style={{ backgroundColor: "#ccc" }}> 
+        <Title variant="h3">Történelmi bejegyzések</Title>
+        {entries.map((entry, index) => (
+          <Entry
+            key={index}
+            title={entry.title}
+            content={entry.content}
+            date={entry.date}
+            author={entry.author}
+          />
+        ))}
       </StyledContainer>
 
-      {mainMenuText && (
-        <StyledContainer>
-          <LargeText variant="body1">{mainMenuText}</LargeText>
-        </StyledContainer>
-      )}
 
-      {subMenuText && (
-        <StyledContainer>
-          <LargeText variant="body1">{subMenuText}</LargeText>
-        </StyledContainer>
-      )}
 
-      {subSubMenuItemText && (
-        <StyledContainer>
-          <LargeText variant="body1">{subSubMenuItemText}</LargeText>
-        </StyledContainer>
-      )}
+
+      
       <CommentSection>
         <CommentHeader>Vélemények és hozzászólások</CommentHeader>
         <CommentInput
