f895bf79fc8920765914b2649caa46de33f93706
SpeedRunHunter <marcell2003.sm@gmail.com>
2024-04-14 19:18:30 +0200

    role frissítő végpont levédése admin-ra

--- backend/src/main/java/hu/speedrundev/sulipedia/config/SpringSecurityConfig.java
+++ backend/src/main/java/hu/speedrundev/sulipedia/config/SpringSecurityConfig.java
@@ -9,6 +9,7 @@ import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -59,6 +60,8 @@ public class SpringSecurityConfig {
           .permitAll()
           .requestMatchers(PUBLIC_URLS)
           .permitAll()
+          .requestMatchers(HttpMethod.PATCH, "/user/{id}")
+          .hasAuthority("ROLE_ADMIN")
           .anyRequest()
           .authenticated()
       )
