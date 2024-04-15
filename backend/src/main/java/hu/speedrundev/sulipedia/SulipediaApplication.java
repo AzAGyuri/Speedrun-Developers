dbc03caf5da48f008ce9d4885f541d262b0799e4
SpeedRunHunter <marcell2003.sm@gmail.com>
2024-04-16 00:23:24 +0200

    swagger ui szerver definíció beállítása

--- backend/src/main/java/hu/speedrundev/sulipedia/SulipediaApplication.java
+++ backend/src/main/java/hu/speedrundev/sulipedia/SulipediaApplication.java
@@ -1,10 +1,20 @@
 package hu.speedrundev.sulipedia;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@OpenAPIDefinition(
+  servers = {
+    @Server(
+      url = "https://api.sulipedia.duckdns.org/swagger-ui/index.html",
+      description = "Sulipédia vizsgaremek projekt Swagger UI backend közvetlen kezelő felülete"
+    ),
+  }
+)
 @SpringBootApplication
 public class SulipediaApplication {
 
