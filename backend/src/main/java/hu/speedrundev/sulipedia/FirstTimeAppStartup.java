1c3faa4885069c296eb6aea08b2f9df35b237a2b
SpeedRunHunter <marcell2003.sm@gmail.com>
2024-04-18 12:03:30 +0200

    commandlineappstartuprunner név túl hosszú; átnevezés FirstTimeAppStartup-ra

--- backend/src/main/java/hu/speedrundev/sulipedia/FirstTimeAppStartup.java
+++ backend/src/main/java/hu/speedrundev/sulipedia/FirstTimeAppStartup.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CommandLineAppStartupRunner implements CommandLineRunner {
+public class FirstTimeAppStartup implements CommandLineRunner {
 
   @Autowired
   private UserRepository userRepository;
